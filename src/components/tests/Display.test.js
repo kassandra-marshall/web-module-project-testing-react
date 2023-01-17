2185fd35ab47d71e20a72a78a6ed78a3073b0345
dyzn3y <kdmarshall728@gmail.com>
2023-01-17 18:27:24 -0500

    made tests for Show and Display

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,64 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
-test('renders Show component when the button is clicked ', () => { });
+const exampleData = {
+    name: "show name",
+    summary: "summary",
+    seasons: [{
+        id: 0,
+        name: "season one",
+        episodes: []
+    },
+    {
+        id: 1,
+        name: "season two",
+        episodes: []
+    }]
+};
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders without errors with no props', () => { 
+    // does this need async? or do I need to be doing something with async?
+    render(<Display />)
+});
+
+test('renders Show component when the button is clicked ', async () => { 
+    mockFetchShow.mockResolvedValueOnce(exampleData);
+
+    render(<Display />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    
+    const show = await screen.findByTestId('show-container')
+    expect(show).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async () => { 
+    mockFetchShow.mockResolvedValueOnce(exampleData);
+
+    render(<Display />);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+    })
+});
+
+test('displayFunc is called when the fetch button is pressed' , async () => {
+    mockFetchShow.mockResolvedValueOnce(exampleData);
+    const displayFunc = jest.fn()
+
+    render(<Display displayFunc={displayFunc}/>);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled();
+    })
+})
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, getByTestId } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import userEvent from '@testing-library/user-event'
 
 // Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and an (empty) list of episodes within them.
 
-// const exampleData = {
-//     name: "show name",
-//     summary: "summary",
-//     seasons: [{
-//         id: 0,
-//         name: "season one",
-//         episodes: []
-//     },
-//     {
-//         id: 1,
-//         name: "season two",
-//         episodes: []
-//     }]
-// }
+const exampleData = {
+    name: "show name",
+    summary: "summary",
+    seasons: [{
+        id: 0,
+        name: "season one",
+        episodes: []
+    },
+    {
+        id: 1,
+        name: "season two",
+        episodes: []
+    }]
+};
 
 test('renders without errors', () => { 
-    // render(<Show show={exampleData} />)
+    render(<Show show={exampleData} selectedSeason={"none"}/>);
 });
 
-test('renders Loading component when prop show is null', () => { });
+test('renders Loading component when prop show is null', () => { 
+    render(<Show show={null} />);
+    const loading = screen.getByTestId("loading-container");
+    expect(loading).toBeInTheDocument();
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders same number of options seasons are passed in', () => { 
+    render(<Show show={exampleData} selectedSeason={"none"} />);
+    const seasonOptions = screen.queryAllByTestId("season-option");
+    expect(seasonOptions).toHaveLength(2);
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('handleSelect is called when an season is selected', () => { 
+    const handleSelect = jest.fn();
+    render(<Show show={exampleData} selectedSeason={"none"} handleSelect={handleSelect}/>);
+    const select = screen.getByLabelText(/Select A Season/i);
+    // userEvent.selectOptions(select, ['1']);
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+    // this works, but why? Why do the instructions say to use userEvent and give documentation, but the test gives fireEvent import?
+    fireEvent.change(select, {target: {value: 2}})
+    expect(handleSelect).toBeCalled();
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { 
+    const { rerender } = render(<Show show={exampleData} selectedSeason={"none"} />)
+    let episodes = screen.queryByTestId("episodes-container");
+    expect(episodes).not.toBeInTheDocument();
+
+    rerender(<Show show={exampleData} selectedSeason={1} />)
+    episodes = screen.queryByTestId("episodes-container");
+    expect(episodes).toBeInTheDocument()
+});
