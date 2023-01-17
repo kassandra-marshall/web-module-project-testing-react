c507cfe6c1083af6e23cda9a765a4278e22e63d6
dyzn3y <kdmarshall728@gmail.com>
2023-01-17 13:39:33 -0500

    made tests for Episode

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,7 +3,26 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-test('renders without errors', () => { });
+// Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and an (empty) list of episodes within them.
+
+// const exampleData = {
+//     name: "show name",
+//     summary: "summary",
+//     seasons: [{
+//         id: 0,
+//         name: "season one",
+//         episodes: []
+//     },
+//     {
+//         id: 1,
+//         name: "season two",
+//         episodes: []
+//     }]
+// }
+
+test('renders without errors', () => { 
+    // render(<Show show={exampleData} />)
+});
 
 test('renders Loading component when prop show is null', () => { });
 
